a8bdbf328f96d163baf84ca77ae3e6bfea96e266
Błażej Kosmowski <blazejkosmowski2@gmail.com>
2017-12-04 20:42:50 +0100

    Initial commit

---
+++ FlashlightToBed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlashlightToBed
+//
+//  Created by Błażej Kosmowski on 04.12.2017.
+//  Copyright © 2017 BLAKOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
